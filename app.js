fa39bdcec8decadcdf6b4c424ef1ce2028753b54
strwbrry05 <jq.strwbrry05@gmail.com>
2025-02-11 20:04:36 -0800

    mobile view base

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const horizontalScroll = document.querySelector('.items');
+horizontalScroll.scrollLeft = (horizontalScroll.scrollWidth - horizontalScroll.clientWidth) / 2;
+
+const highlightPoint = (horizontalScroll.scrollWidth - horizontalScroll.clientWidth) / 2;
+
