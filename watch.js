c0c0a023022509a23ad9f42a57975578e18bc8b6
strwbrry05 <jq.strwbrry05@gmail.com>
2025-02-12 19:12:11 -0800

    mobile view video page

--- watch.js
+++ watch.js
@@ -3,3 +3,18 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const videoType = urlParams.get('videos');
 console.log(videoType);
+
+
+const player = document.querySelector('.player');
+
+selectVideos(videoType);
+
+async function selectVideos(videoType) {
+    const res = await fetch('videos.json');
+    const data = await res.json();
+    console.log(data);
+    console.log(data[videoType]);
+    const videoTypeArr = data[videoType];
+
+    player.src = `${videoTypeArr[0]}`;
+}
