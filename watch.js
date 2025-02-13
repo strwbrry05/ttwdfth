70ea6209bdd415e9b6426c6e759a2fb52dbdb1b6
strwbrry05 <jq.strwbrry05@gmail.com>
2025-02-12 20:43:35 -0800

    mobile video testing

--- watch.js
+++ watch.js
@@ -6,15 +6,57 @@ console.log(videoType);
 
 
 const player = document.querySelector('.player');
+const holder = document.querySelector('.videoHolder');
+let count = 0;
+
 
 selectVideos(videoType);
+
 
 async function selectVideos(videoType) {
     const res = await fetch('videos.json');
     const data = await res.json();
-    console.log(data);
     console.log(data[videoType]);
+
     const videoTypeArr = data[videoType];
 
-    player.src = `${videoTypeArr[0]}`;
+    displayVideo(videoTypeArr);
+
+    document.addEventListener('click', (e) => {
+        count++;
+        if (count === 4) {
+            count = 0;
+        }
+        displayVideo(videoTypeArr);
+        console.log('next!');
+    });
+
 }
+
+function displayVideo(videoTypeArr) {  
+    let videoInfo = videoTypeArr[count];
+    let videoLink = videoInfo[0];
+    let videoHeight = videoInfo[1];
+    
+    if (videoHeight === 1000) {
+        holder.classList.remove('smallsize');
+        player.classList.remove('smallVideo');
+        holder.classList.add('fullsize');
+        player.classList.add('bigVideo');
+    }
+    else if (videoHeight === 900) {
+        holder.classList.remove('fullsize');
+        player.classList.remove('bigVideo');
+        holder.classList.add('smallsize');
+        player.classList.add('smallVideo');
+    }
+    player.src = videoLink;
+}
+
+// document.addEventListener('mousedown', (e) => {
+//     console.log('hi');
+// })
+
+// document.addEventListener('mouseup', (e) => {
+//     console.log('bye');
+// })
